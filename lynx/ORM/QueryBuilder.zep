ccd3f2ec1bb09ae9b457b4a6ea41bde0df337bbd
Дмитрий Пацура <zaets28rus@gmail.com>
2014-03-25 15:15:58 +0400

    Travic build delete not need install preregs

--- lynx/ORM/QueryBuilder.zep
+++ lynx/ORM/QueryBuilder.zep
@@ -55,7 +55,7 @@ class QueryBuilder
 
 	public function join()
 	{
-	    
+
 	}
 
 	public function limit(int limit)
--- tests/ci/install_prereqs.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get -qq install re2c
-git clone https://github.com/json-c/json-c
-cd json-c
-sh autogen.sh
-./configure --silent
-make --silent
-sudo make --silent install
