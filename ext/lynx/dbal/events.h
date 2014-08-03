23760541fde36cbf506d8f88ea0feeff28096b3e
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-03 16:20:50 +0400

    Remove oldest c generated code

--- ext/lynx/dbal/events.h
+++
@@ -1,5 +0,0 @@
-
-extern zend_class_entry *lynx_dbal_events_ce;
-
-ZEPHIR_INIT_CLASS(Lynx_DBAL_Events);
-
--- ext/lynx/stdlib/eventmanager.h
+++
@@ -1,5 +0,0 @@
-
-extern zend_class_entry *lynx_stdlib_eventmanager_ce;
-
-ZEPHIR_INIT_CLASS(Lynx_Stdlib_EventManager);
-
