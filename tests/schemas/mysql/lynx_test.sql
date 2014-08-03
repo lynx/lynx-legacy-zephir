5eb42434cc8df8f55a8ae6fa0aef46aebddff686
Дмитрий Пацура <zaets28rus@gmail.com>
2014-08-03 16:14:44 +0400

    Update schema
    - Add groups table
    
    - Add group_id to users table

--- tests/schemas/mysql/lynx_test.sql
+++ tests/schemas/mysql/lynx_test.sql
@@ -1,50 +1,99 @@
--- phpMyAdmin SQL Dump
--- version 4.1.3
--- http://www.phpmyadmin.net
+-- MySQL dump 10.13  Distrib 5.5.38, for debian-linux-gnu (i686)
 --
--- Хост: localhost
--- Время создания: Апр 26 2014 г., 18:01
--- Версия сервера: 5.5.35-33.0
--- Версия PHP: 5.5.9-1ubuntu4
+-- Host: localhost    Database: lynx_test
+-- ------------------------------------------------------
+-- Server version	5.5.38-0ubuntu0.14.04.1
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- База данных: `lynx_test`
+-- Table structure for table `groups`
 --
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `11` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  PRIMARY KEY (`11`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Структура таблицы `products`
+-- Dumping data for table `groups`
 --
 
-CREATE TABLE IF NOT EXISTS `products` (
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (1,'Пользователь'),(2,'Бета Пользователь');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `products`
+--
+
+DROP TABLE IF EXISTS `products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Структура таблицы `users`
+-- Dumping data for table `products`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+LOCK TABLES `products` WRITE;
+/*!40000 ALTER TABLE `products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) NOT NULL,
-  `group_id` INT(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Дамп данных таблицы `users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `group_id`) VALUES
-(1, 'Дмитрий', 1),
-(2, 'Илья', 2),
-(3, 'Денис', 2),
-(4, 'Елисей', 2);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Дмитрий',1),(2,'Илья',0),(3,'Денис',0),(4,'Елисей',0),(5,'Test string',1),(6,'Test string',1),(7,'Test string',1),(8,'Test string',1),(9,'Test string',1),(10,'Test string',1),(11,'Test string',1),(12,'Test string',1),(13,'Test string',1),(14,'Test string',1),(15,'Test string',1),(16,'Test string',1),(17,'Test string',1),(18,'Test string',1),(19,'Test string',1),(20,'Test string',1),(21,'Test string',1),(22,'Test string',1),(23,'Test string',1),(24,'Test string',1),(25,'Test string',1),(26,'Test string',1),(27,'Test string',1),(28,'Test string',1),(29,'Test string',1),(30,'Test string',1),(31,'Test string',1),(32,'Test string',1),(33,'Test string',1),(34,'Test string',1),(35,'Test string',1),(36,'Test string',1),(37,'Test string',1),(38,'Test string',1),(39,'Test string',1),(40,'Test string',1),(41,'Test string',1),(42,'Test string',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-08-02 18:16:29
