ad7462e2bac693a99dc210c8c52cba0d6f932aa2
ovr <zaets28rus@gmail.com>
2014-10-17 19:34:05 +1000

    Tests Utils support Pgsql Driver

--- tests/Lynx/Tests/Utils.php
+++ tests/Lynx/Tests/Utils.php
@@ -15,7 +15,20 @@ class Utils
 	{
         $eventsManager = new Manager();
 
-		$driver = new DBAl\Driver\Mysql('mysql:host=localhost;dbname='.$GLOBALS['db_name'], $GLOBALS['db_username'], $GLOBALS['db_password'], array());
+        switch ($GLOBALS['db_type']) {
+            case 'mysql':
+            case 'pdo_mysql':
+                $driver = new DBAl\Driver\Mysql('mysql:host=localhost;dbname='.$GLOBALS['db_name'], $GLOBALS['db_username'], $GLOBALS['db_password'], array());
+                break;
+            case 'pgsql':
+            case 'pdo_pgsql':
+                $driver = new DBAl\Driver\Pqsql('pgsql:host=localhost;dbname='.$GLOBALS['db_name'], $GLOBALS['db_username'], $GLOBALS['db_password'], array());
+                break;
+            default:
+                throw new \InvalidArgumentException('Unsupported db type : ' . $GLOBALS['db_type']);
+                break;
+        }
+
 		$connection = new DBAL\Connection(['driver' => $driver], $eventsManager);
 
 		return $connection;
