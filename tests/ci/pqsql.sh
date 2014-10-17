65429f256fab5016486e7098ec7f89af8a01cf1c
ovr <zaets28rus@gmail.com>
2014-10-17 19:48:44 +1000

    Try to fix pgsql.sh ci install script

--- tests/ci/pqsql.sh
+++ tests/ci/pqsql.sh
@@ -1,2 +1,2 @@
-psql -c 'create database lynx_test;'
-pg_dump -Fc lynx_test > ./tests/schemas/pqsql/lynx_test.sql
+psql -c 'create database lynx_test;' -U postgres
+pg_dump -Fc lynx_test > ./tests/schemas/pqsql/lynx_test.sql -U postgres
